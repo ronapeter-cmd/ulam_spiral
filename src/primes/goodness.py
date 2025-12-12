e6c53edfed7fbc1ca72663a1e4cd123b36416e5f
ronapeter-cmd <rona.peter@gmail.com>
2025-12-12 11:09:40 +0100

    Update goodness.py

--- src/primes/goodness.py
+++ src/primes/goodness.py
@@ -191,7 +191,7 @@ def detect_horizontal_vertical_segments (matrix: np.ndarray, gap_tolerance:int=1
               j = k
               if 0 <= i < rows and 0 <= j < cols:
                   vertical_mask[i,j] = 1
-                  mask |= vertical_mask
+  mask |= vertical_mask
     
   return mask
 
