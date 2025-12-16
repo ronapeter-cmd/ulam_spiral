7e4955b3b0ac9aa31214ee594dda6614f7c91f79
ronapeter-cmd <rona.peter@gmail.com>
2025-12-16 10:47:19 +0100

    Update __init__.py

--- src/primes/__init__.py
+++ src/primes/__init__.py
@@ -1,10 +1,11 @@
 from .primes import is_prime
 from .ulam_spiral import fill_primes, set_if_inside
-from .viz import display_primes, show_with_diagonals
-from .goodness import ulam_goodness, detect_diagonal_segments
+from .viz import display_primes, show_with_segments
+from .goodness import ulam_goodness, detect_diagonal_segments, detect_horizontal_vertical_segments
 
 __all__ = [
     "is_prime", "fill_primes", "set_if_inside",
-    "display_primes", "show_with_diagonals",
-    "ulam_goodness", "detect_diagonal_segments"
+    "display_primes", "show_with_segments",
+    "ulam_goodness", "detect_diagonal_segments",
+    "detect_horizontal_vertical_segments"
 ]
